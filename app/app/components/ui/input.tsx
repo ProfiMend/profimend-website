ce279e5fac3910eafaa421cba9148d228eb5ee03
ProfiMend <danielpat@tutanota.com>
2025-10-19 18:59:31 -0600

    Create input.tsx

---
+++ app/app/components/ui/input.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+export function Input({className="", ...props}: React.InputHTMLAttributes<HTMLInputElement>) {
+  return (
+    <input
+      className={
+        "w-full rounded-lg border px-3 py-2 text-sm outline-none focus:ring-2 focus:ring-sky-200 " +
+        className
+      }
+      {...props}
+    />
+  );
+}
