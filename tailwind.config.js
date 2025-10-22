91f2923d0c156dc4b6f3f750c29bc136ce85ce37
ProfiMend <danielpat@tutanota.com>
2025-10-22 13:45:08 -0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/**/*.{ts,tsx,js,jsx}"
-  ],
+  content: ["./app/**/*.{ts,tsx,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        pmnavy: "#0b2240",
+        pmsky: "#0284c7",
+        pmteal: "#10b981",
+        pmgold: "#f59e0b",
+        pmorange: "#f97316",
+      },
+    },
   },
   plugins: [],
 };
+
