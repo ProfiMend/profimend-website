a2883fb643104619f8cfbc2a765a343c9b4d9c3b
ProfiMend <danielpat@tutanota.com>
2025-10-22 13:59:55 -0600

    Update input.tsx

--- app/components/ui/input.tsx
+++ app/components/ui/input.tsx
@@ -3,8 +3,8 @@ export function Input({className="", ...props}: React.InputHTMLAttributes<HTMLIn
   return (
     <input
       className={
-        "w-full rounded-lg border px-3 py-2 text-sm outline-none focus:ring-2 focus:ring-sky-200 " +
-        className
+        "w-full rounded-lg border border-slate-300 px-3 py-2 text-sm " +
+        "outline-none focus:ring-2 focus:ring-sky-200 " + className
       }
       {...props}
     />
